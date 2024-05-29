59b3108b3ae04a0c1b3547806fdd8def7d4bbe67
Geoffrey2002 <gmwangi3174@gmail.com>
2024-05-29 17:04:09 +0300

    added the not found pages

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import supabase from "./supabase";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -16,6 +17,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
@@ -30,6 +32,7 @@ export async function getCabinPrice(id) {
 
   if (error) {
     console.error(error);
+    
   }
 
   return data;
